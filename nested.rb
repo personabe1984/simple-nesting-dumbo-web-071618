a9d4fcb7dc7d99a67ce9b265ebb157dab5a4bc69
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-07 23:28:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  return 
 end
 
 def adding_matz
